d06476535fd2d71eeac8d0c9427d7b66cad41b63
Mathe <mathee@NIAMS01763000A.local>
2014-01-03 14:24:39 -0500

    First working website 01/03/13

---
+++ rakefile.rb
@@ -0,0 +1,28 @@
+#Rakefile
+#require 'nanoc3/tasks'
+
+require 'rubygems'
+
+desc "Publish site to Github"
+task :publish do
+ src = 'output/'
+ dst = 'casellaslab.git.io/'
+
+ puts '==== Syncing working directory...'
+ sh("rsync -arv --delete --exclude '.git' #{src} #{dst}")
+ 
+ puts '==== Commiting changed files'
+ sh("cd #{dst}; git init; git add *")
+ 
+ commit_message = "Update website: " #+ issue.updated_at.to_datetime.rfc3339   #(string)Time.now
+ sh("cd #{dst}; git commit -m '#{commit_message}'")
+
+ puts "Adding remote locations Casellaslab/casellaslab.github.io"
+ sh("cd #{dst};git remote; git remote add origin https://github.com/CasellasLab/CasellasLab.github.io")
+
+ puts '==== Pushing changes to Github'
+ sh("cd #{dst}; git push --force origin master")
+
+ puts '==== Published website to Github'
+
+end
